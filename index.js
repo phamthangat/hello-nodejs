0443aad43e59b6640a00291f50feccd66e7906fb
thang pham <phamthangat397@gmail.com>
2022-12-04 23:36:52 +0700

    add route

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const port = 3000
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+app.get('/hello', (req, res) => {
+  res.send('Xin chao')
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
